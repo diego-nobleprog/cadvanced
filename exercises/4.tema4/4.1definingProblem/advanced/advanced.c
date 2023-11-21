92f713088be46e2f84f2f6e9479757d59030b08f
diego-nobleprog <diegonobleprog@gmail.com>
2023-11-21 05:59:30 -0600

    Exercices 4.1 are added

--- exercises/4.tema4/4.1definingProblem/advanced/advanced.c
+++ exercises/4.tema4/4.1definingProblem/advanced/advanced.c
@@ -1,17 +1,63 @@
-#include <stdio.h>
+#include <stdio.h>      // Para funciones de entrada y salida estándar.
+#include <stdlib.h>     // Para la función exit y otras.
+#include <string.h>     // Para funciones de manejo de strings.
+#include <unistd.h>     // Para funciones POSIX, como read y write.
+#include <sys/socket.h> // Para funciones de sockets.
+#include <netinet/in.h> // Para estructuras de direcciones de dominio de Internet.
+#include <time.h>       // Para funciones de manejo de tiempo.
+
+#define PORT 8080       // Puerto donde el servidor estará escuchando.
 
 int main() {
-    int numero1, numero2, suma;
-
-    printf("Introduce el primer número: "); // Pide al usuario el primer número
-    scanf("%d", &numero1); // Lee el primer número
-
-    printf("Introduce el segundo número: "); // Pide al usuario el segundo número
-    scanf("%d", &numero2); // Lee el segundo número
-
-    suma = numero1 + numero2; // Suma los dos números
-
-    printf("La suma de %d y %d es %d.\n", numero1, numero2, suma); // Imprime el resultado
-
-    return 0; // Termina el programa con éxito
+    int server_fd, new_socket;                     // Descriptores de archivo para el servidor y la nueva conexión de socket.
+    struct sockaddr_in address;                    // Estructura para la dirección del servidor.
+    int addrlen = sizeof(address);                 // Tamaño de la estructura de dirección.
+    char buffer[1024] = {0};                       // Buffer para almacenar la hora.
+    time_t current_time;                           // Variable para almacenar la hora actual.
+    
+    // Creación del socket del servidor.
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Definición de la dirección del servidor.
+    address.sin_family = AF_INET;                  // Tipo de dirección para sockets de red.
+    address.sin_addr.s_addr = INADDR_ANY;          // Acepta conexiones en cualquier IP del servidor.
+    address.sin_port = htons(PORT);                // Convierte el número de puerto al orden de bytes de red.
+    
+    // Vinculación del socket a la dirección del servidor.
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Poner al servidor a escuchar conexiones entrantes.
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Aceptar una conexión entrante.
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Obtención de la hora actual.
+    time(&current_time);
+    struct tm *time_info = localtime(&current_time); // Convertir la hora a la hora local.
+    
+    // Formatear la hora en un string y enviarla al cliente.
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", time_info); // Formatear la hora y la fecha.
+    send(new_socket, buffer, strlen(buffer), 0); // Enviar la hora al cliente.
+    printf("Hora enviada al cliente: %s\n", buffer); // Imprimir en consola la hora enviada.
+    
+    // Cerrar la conexión.
+    close(new_socket);
+    
+    // Cerrar el servidor.
+    close(server_fd);
+    
+    return 0; // Termina el programa con éxito.
 }
--- exercises/4.tema4/4.1definingProblem/practice/practice.c
+++ exercises/4.tema4/4.1definingProblem/practice/practice.c
@@ -1,17 +1,46 @@
 #include <stdio.h>
 
+// Prototipos de funciones para las conversiones
+float celsiusToFahrenheit(float celsius);
+float fahrenheitToCelsius(float fahrenheit);
+
+// Función principal
 int main() {
-    int numero1, numero2, suma;
+    int opcion;
+    float temperatura, convertida;
 
-    printf("Introduce el primer número: "); // Pide al usuario el primer número
-    scanf("%d", &numero1); // Lee el primer número
+    // Mostrar menú de opciones al usuario
+    printf("Conversor de Temperaturas\n");
+    printf("1. Celsius a Fahrenheit\n");
+    printf("2. Fahrenheit a Celsius\n");
+    printf("Elige una opción: ");
+    scanf("%d", &opcion);
 
-    printf("Introduce el segundo número: "); // Pide al usuario el segundo número
-    scanf("%d", &numero2); // Lee el segundo número
+    // Leer la temperatura a convertir
+    printf("Ingresa la temperatura: ");
+    scanf("%f", &temperatura);
 
-    suma = numero1 + numero2; // Suma los dos números
-
-    printf("La suma de %d y %d es %d.\n", numero1, numero2, suma); // Imprime el resultado
+    // Realizar la conversión basada en la opción
+    if (opcion == 1) {
+        convertida = celsiusToFahrenheit(temperatura);
+        printf("%.2f grados Celsius son %.2f grados Fahrenheit.\n", temperatura, convertida);
+    } else if (opcion == 2) {
+        convertida = fahrenheitToCelsius(temperatura);
+        printf("%.2f grados Fahrenheit son %.2f grados Celsius.\n", temperatura, convertida);
+    } else {
+        printf("Opción no válida.\n");
+        return 1; // Termina el programa con un código de error
+    }
 
     return 0; // Termina el programa con éxito
+}
+
+// Definición de la función celsiusToFahrenheit
+float celsiusToFahrenheit(float celsius) {
+    return (celsius * 9 / 5) + 32; // Fórmula de conversión de Celsius a Fahrenheit
+}
+
+// Definición de la función fahrenheitToCelsius
+float fahrenheitToCelsius(float fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9; // Fórmula de conversión de Fahrenheit a Celsius
 }
