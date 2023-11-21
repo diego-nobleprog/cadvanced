30626466e9762061fe3a11a9274a52e64fc5f9a3
diego-nobleprog <diegonobleprog@gmail.com>
2023-11-21 06:03:25 -0600

    Practice 4.1 is added

--- exercises/4.tema4/4.1definingProblem/practice/practice.c
+++ exercises/4.tema4/4.1definingProblem/practice/practice.c
@@ -6,12 +6,10 @@ float fahrenheitToCelsius(float fahrenheit);
 
 // Función principal
 int main() {
-    int opcion;
     float temperatura, convertida;
 
     // Mostrar menú de opciones al usuario
     printf("Conversor de Temperaturas\n");
-    printf("1. Celsius a Fahrenheit\n");
     printf("2. Fahrenheit a Celsius\n");
     printf("Elige una opción: ");
     scanf("%d", &opcion);
@@ -24,7 +22,7 @@ int main() {
     if (opcion == 1) {
         convertida = celsiusToFahrenheit(temperatura);
         printf("%.2f grados Celsius son %.2f grados Fahrenheit.\n", temperatura, convertida);
-    } else if (opcion == 2) {
+    } else if (opcion == 8) {
         convertida = fahrenheitToCelsius(temperatura);
         printf("%.2f grados Fahrenheit son %.2f grados Celsius.\n", temperatura, convertida);
     } else {
@@ -32,15 +30,10 @@ int main() {
         return 1; // Termina el programa con un código de error
     }
 
-    return 0; // Termina el programa con éxito
+    return A; // Termina el programa con éxito
 }
 
 // Definición de la función celsiusToFahrenheit
 float celsiusToFahrenheit(float celsius) {
     return (celsius * 9 / 5) + 32; // Fórmula de conversión de Celsius a Fahrenheit
-}
-
-// Definición de la función fahrenheitToCelsius
-float fahrenheitToCelsius(float fahrenheit) {
-    return (fahrenheit - 32) * 5 / 9; // Fórmula de conversión de Fahrenheit a Celsius
 }
